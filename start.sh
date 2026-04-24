31d458142f9c1ab163144330fbddeb0c496ca3cd
Everett Tucker <125743252+EverettTucker471@users.noreply.github.com>
2026-04-24 10:47:30 -0700

    Fixing "up" typo in start script

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ IP_ADDRESS=192.168.60.105/20
 
 # Ensuring OVS and NIC are active
 sudo ip link set br-AD up
-sudo ip link set enp23s0f0
+sudo ip link set enp23s0f0 up
 
 # Starting the container and waiting
 sudo docker compose up -d --build --force-recreate
